c788d8865310bdf69343161936ea93d70fefc7d1
snwfdhmp <snwfdhmp@protonmail.com>
2017-03-22 00:08:09 +0100

    Currently building own MVC architecture (Authenticator class). Project should not be used now

--- app/index.php
+++ app/index.php
@@ -1,6 +1,24 @@
 <?php
 session_start();
 
+// Following GLOBALS configuration is inspired by phpSteroid (https://github.com/DarKnight1346/phpSteroid/blob/master/index.php)
+$GLOBALS["config"] = array(
+	"appName" => "ICS",
+	"version" => "beta 0.1",
+	"domain" => "localhost:8888",
+	"authenticator" => array(
+		"sessionCookieLength" => 40, //length of the connexion cookie
+		"connexionTimeout" => 5*3600 //if time-lastPingTime > this, ask for a reconnexion
+		),
+	"database" => array(
+		"host"=>"localhost",
+		"username"=>"dev",
+		"password"=>"dev",
+		"name"=>"ICS"
+		),
+
+	);
+
 $app_urls = array (
 	"accueil" => "accueil.php",
 	"calendar" => "calendar.php",
@@ -19,6 +37,7 @@ $actions = "../actions/";
 if(!isset($_GET["u"])) {
 	$_GET["u"] = "accueil";
 }
+
 if(array_key_exists($_POST["action"], $app_actions)) {
 	include("../actions/proceedLogin.php");
 }
---
+++ lib/class/Authenticator.php
@@ -0,0 +1,72 @@
+<?
+class Authenticator {
+	private $authenticated;
+	private $email;
+	private $id_user;
+
+	public function isAuthenticated() { //returns whether the current user is authenticated or not
+		return $this->authenticated;
+	}
+
+	public function setEmail($newEmail) { //set object's email
+	if(isEmail($newEmail)) {
+		$this->email = $newEmail;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+	private function generateSessionCookie() { //generateSessionCookie
+		return bin2hex(random_bytes($GLOBALS["config"]["authenticator"]["sessionCookieLength"]));
+	}
+
+	public function writeConnexion($pass) {
+		if(strlen($pass) >= 8) {
+			$query = $db->prepare("SELECT * FROM users WHERE email=:email");
+			$query->bindParam(':email', $email);
+			$query->execute();
+			$answer = $query->fetch();
+
+			if (password_verify($pass, $answer["pass"]))
+			{
+				return $answer["id_user"];
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;
+
+		}
+	}
+
+	public function requiresAuthentication() {
+
+	}
+
+	public function verifyAuthenticated() {
+		if(!isset($_SESSION['sessionCookie']))
+			return false;
+		$sessionCookie = $_SESSION['sessionCookie'];
+		if(!isCookieCorrect($sessionCookie))
+			return false;
+
+
+	}
+
+	private function isCookieCorrect($cookie, $id_user, $ip) {
+		$query = $db->prepare("SELECT * FROM connexions WHERE sessionCookie=:cookie AND id_user=:id_user AND last_ip");
+		$query->bindParam(':cookie', $cookie);
+		$query->bindParam(':id_user', $id_user);
+		$nbRows = $query->execute()->fetchColumn();
+
+		if($nbRows <= 0) //if there's no entry in db, don't go forward
+			return false;
+
+		$query = $db->prepare("")
+	}
+
+}
